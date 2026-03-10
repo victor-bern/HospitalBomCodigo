bc351a6e0984042d3aba5c7d2cc9982617947938
Victor <dartinjs@gmail.com>
2026-03-10 20:53:53 -0300

    Adicionando modelo do exame

---
+++ HospitalBomCodigo/Models/Exam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalBomCodigo.Models
+{
+    public class Exam
+    {
+        public Guid Id { get; private set; }
+        public string Type { get; set; }
+        public string? Description { get; private set; }
+        public ExamStatus Status { get; private set; } = ExamStatus.Pending;
+
+        public bool NotificationSent { get; private set; }
+        public DateTime? ResultReleasedAt { get; private set; }
+
+    }
+
+    public enum ExamStatus
+    {
+        Pending = 1,
+        Released = 2
+    }
+}
+
