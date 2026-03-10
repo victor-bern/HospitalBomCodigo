836820c9a87b442e30810714dcffb2e72218ecdf
Victor <dartinjs@gmail.com>
2026-03-10 20:58:21 -0300

    Adicionando sqlite ao projeto

---
+++ HospitalBomCodigo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalBomCodigo.Context
+{
+    public class AppDbContext(DbContextOptions opt) : DbContext(opt)
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=HospitalBomCodigo.db");
+        }
+    }
+}
