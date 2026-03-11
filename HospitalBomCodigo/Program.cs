9b75d5284bbd5f53d2a3ef67ca44faf0707de197
Victor <dartinjs@gmail.com>
2026-03-10 21:05:04 -0300

    Garantindo que irá recriar o banco sempre que iniciar

--- HospitalBomCodigo/Program.cs
+++ HospitalBomCodigo/Program.cs
@@ -1,3 +1,12 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using HospitalBomCodigo.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+using var host = builder.Build();
+
+using var scope = host.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
